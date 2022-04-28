281150f3fb805640bca0b6f631d1923b49e6db14
Patryk Popowicz <ptrykppowicz@gmail.com>
2022-04-29 01:02:47 +0200

    add bday counter
    add bday counter
    clean up the code
    fix ben join crash

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require("dotenv").config()
 const Discord = require("discord.js")
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice")
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", Discord.Intents.FLAGS.GUILD_VOICE_STATES] })
+const client = new Discord.Client({ intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES
+  ] 
+})
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -11,18 +16,22 @@ client.on("ready", () => {
 
 client.on("messageCreate", msg => {
   let msgArgs = msg.content.toLowerCase().split(" ")
+  if (msgArgs.includes("doxx")) {
+    doxx(msg)
+    return
+  }
+
   if (msgArgs.includes("ben") || msgArgs.includes("ben?")) {
     if (msgArgs.includes("join")) {
       voiceJoin(msg)
       return
-    } else if (msgArgs.includes("leave")) {
+    } 
+    if (msgArgs.includes("leave")) {
       voiceLeave(msg)
       return
     }
 
     ben(msg)
-  } else if (msgArgs.includes("doxx")) {
-    doxx(msg)
   }
 })
 
@@ -33,11 +42,11 @@ function ben(msg) {
   msg.channel.send(responses[Math.floor(Math.random() * responses.length)])
 }
 
-var doxxed = {}
-
 function random255() {
   return Math.floor(Math.random() * 254 + 1)
 }
+
+var doxxed = {}
 
 function doxx(msg) {
   let mentions = Array.from(msg.mentions.users)
@@ -61,7 +70,7 @@ function doxx(msg) {
 
   let ip = [random255(), random255(), random255(), random255()].join(".")
   doxxed[targetId] = ip
-  msg.channel.send(ip)
+  msg.channel.send("`" + ip + "`")
 }
 
 var connection
@@ -69,10 +78,17 @@ const player = createAudioPlayer()
 
 function voiceJoin(msg) {
   var voiceChannel = msg.member.voice.channel
+
+  if (!voiceChannel) {
+    msg.channel.send("no")
+    return
+  }
+
   if (voiceChannel.members.has(client.user.id)) {
     msg.channel.send("im already talking there")
     return
   }
+
   connection = joinVoiceChannel({
     channelId: voiceChannel.id,
     guildId: msg.guild.id,
@@ -91,14 +107,22 @@ function voiceLeave(msg) {
   }
 }
 
+const dirthday = Date.parse("June 18, 2022")
+
 function setPresence() {
+  let now = Date.now()
+  let timeUntilBirthday = dirthday - now
+  let timeParseDays = Math.floor(timeUntilBirthday / 1000 / 60 / 60 / 24)
   client.user.setActivity(
-    "my talking ben",
+    timeParseDays.toString() + "d till 18!",
     {
       type: "STREAMING",
       url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     }
   )
+  console.log(new Date().toLocaleString().replace(',','') + " - updated presence")
 }
+
+setInterval(setPresence, 1000 * 60 * 60)
 
 client.login(process.env.CLIENT_TOKEN)
